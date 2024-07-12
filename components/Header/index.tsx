be74e413906733aa3a535cd43a5060bbd31d12a4
MienPV <mien.pham@vexere.com>
2024-07-12 20:21:09 +0700

    fix header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useColorMode } from "@chakra-ui/react";
-import Link from "next/link";
-import Icon from "@/components/Icon";
-import Modal from "@/components/Modal";
-import Trade from "@/components/Trade";
-import User from "@/components/User";
 import Image from "@/components/Image";
-import Notifications from "./Notifications";
-import Search from "./Search";
 
 type HeaderProps = {
     visible?: boolean;
@@ -38,56 +31,8 @@ const Header = ({visible, showMenu, onClickBurger }: HeaderProps) => {
                             ABS SCAN
                         </div>
                     </div>
-
-                    <div className="flex items-center ml-auto space-x-6">
-                        <button
-                            className="btn-primary md:hidden"
-                            onClick={() => setVisibleModalTrade(true)}
-                        >
-                            Buy & Sell
-                        </button>
-                        <button
-                            className="group w-12 h-12 outline-none md:w-8 md:h-8"
-                            onClick={() => setVisibleModalSearch(true)}
-                        >
-                            <Icon
-                                className="fill-theme-secondary transition-colors group-hover:fill-theme-primary"
-                                name="search"
-                            />
-                        </button>
-                        <Notifications />
-                        <button
-                            className={`hidden rounded-full transition-shadow md:block ${
-                                showMenu ? "shadow-[0_0_0_0.125rem_#0C68E9]" : ""
-                            }`}
-                            onClick={onClickBurger}
-                        >
-                            <Image
-                                className="w-8 h-8 object-cover rounded-full opacity-100"
-                                src="/images/avatar-1.jpg"
-                                width={32}
-                                height={32}
-                                alt=""
-                            />
-                        </button>
-                        <User className="md:hidden" />
-                    </div>
                 </div>
             </div>
-            <Modal
-                classWrap="max-w-[40rem] !p-0 rounded-3xl overflow-hidden"
-                visible={visibleModalSearch}
-                onClose={() => setVisibleModalSearch(false)}
-            >
-                <Search />
-            </Modal>
-            <Modal
-                classWrap="p-8 md:!px-4 md:!py-6"
-                visible={visibleModalTrade}
-                onClose={() => setVisibleModalTrade(false)}
-            >
-                <Trade />
-            </Modal>
         </>
     );
 };
